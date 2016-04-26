249bd92b5bd57d590209a6fe8abd340ea1a27b4d
Hack Reactor Students <accounts@hackreactor.com>
2016-04-26 12:32:26 -0700

    Progress

--- src/predator.js
+++ src/predator.js
@@ -1,11 +1,41 @@
 var Predator = function() {
   Fish.call(this);
   this.$node.addClass('predator');
-  
+
   this.height *= 2;
   this.width *= 2;
+
+  this.hungry = true;
+
+  this.repaint();
 }
 
 Predator.prototype = Object.create(Fish.prototype);
 
 Predator.prototype.constructor = Predator;
+
+Predator.prototype.move = function(interval) {
+  // debugger;
+  if (this.hungry) {
+    var nearestFish = this.findNearestFish();
+    console.log(nearestFish);
+    this.repaint();
+  } else {
+    Fish.prototype.move.call(this, interval);
+  }
+};
+
+Predator.prototype.findNearestFish = function() {
+  var nearestDistance = Number.POSITIVE_INFINITY;
+  var nearestFish;
+
+  window.fishes.forEach(function(fish) {
+    var distance = Math.sqrt( Math.pow((this.x - fish.x), 2) + Math.pow((this.y - fish.y), 2));
+    if (distance < nearestDistance && fish !== this) {
+      nearestDistance = distance;
+      nearestFish = fish;
+    }
+  });
+
+  return nearestFish;
+};
