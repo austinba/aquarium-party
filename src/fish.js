681dbb986c1430a23255a569f01019757bb178f4
Hack Reactor Students <accounts@hackreactor.com>
2016-04-26 11:55:42 -0700

    Reorganizing movement functionality

--- src/fish.js
+++ src/fish.js
@@ -2,15 +2,33 @@ var Fish = function() {
   this.$node = $('<div class="fish">FISH</div>');
   this.width = 100;
   this.height = 30;
+
+  this.x = this.widthAvailable() * Math.random() + this.width / 2;
+  this.y = this.heightAvailable() * Math.random() + this.height / 2;
+
   this.speed = 50 + 4 * (1 - Math.random()*2); // pixels per second
   this.direction = Math.random() < 0.5 ? -1 : 1;
+
+  // bobbing effect
+  this.bobCycle = 0;  // radians
+  this.bobSpeed = Math.random() * 3;
+  this.bobHeight = Math.random() * 5 + 2;
+  this.bobOffset= 0;
+
   this.$node.addClass('fish-bob' + Math.floor(Math.random() * 3 + 1));
   if(this.direction < 0) {
     this.$node.css('transform', 'scaleX(-1)');
   }
-  this.left = this.widthAvailable() * Math.random();
-  this.top = this.heightAvailable() * Math.random();
+
   this.repaint();
+};
+
+Fish.prototype.left = function() {
+  return this.x - (this.width / 2);
+};
+
+Fish.prototype.top = function() {
+  return this.y - (this.width / 2) + this.bobOffset;
 };
 
 Fish.prototype.widthAvailable = function() {
@@ -21,21 +39,39 @@ Fish.prototype.heightAvailable = function() {
 }
 
 Fish.prototype.move = function(interval) {
+  
   // change direction on edge collison
-  if(this.left < 0) {
+  var currentTransform = this.$node.css('transform');
+  if(this.left() < 0) {
     this.direction = 1;
     this.$node.css('transform', 'scaleX(1)');
-  } else if(this.left > this.widthAvailable()) {
+  } else if(this.left() > this.widthAvailable()) {
     this.direction = -1;
     this.$node.css('transform', 'scaleX(-1)');
   }
-  this.left += this.direction * this.speed * interval;
 
+  // bob
+  this.bobCycle += this.bobSpeed * interval;
+  this.bobOffset = this.bobHeight * Math.sin(this.bobCycle);
+
+  this.x += this.direction * this.speed * interval;
   this.repaint();
 };
+
+Fish.prototype.bob = function() {
+  // bobbing effect
+  
+  this.top = this.bobHeight * Math.sin(this.bobCycle);
+  //var bobPrevHeight = this.bobOffset;
+
+
+  //this.bobOffset = this.bobHeight * Math.sin(this.bobCycle);
+  //this.top += this.bobOffset - bobPrevHeight;
+};
+
 Fish.prototype.repaint = function() {
   this.$node.css('width', this.width);
   this.$node.css('height', this.height);
-  this.$node.css('left', this.left);
-  this.$node.css('top', this.top);
+  this.$node.css('left', this.left());
+  this.$node.css('top', this.top());
 }
