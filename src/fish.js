a2d71bfab6a7150624fe2865cd0123bf32734424
Hack Reactor Students <accounts@hackreactor.com>
2016-04-25 23:11:36 -0700

    Updated Fish Positioning

--- src/fish.js
+++ src/fish.js
@@ -1,15 +1,17 @@
 var Fish = function() {
-  this.$node = this.$createNode();
   this.width = 100;
   this.height = 30;
+  this.$node = this.$createNode();
 };
 Fish.prototype.$createNode = function() {
-  var aquariumWidth = $aquarium.width();
-  var aquariumHeight = $aquarium.height();
+
+  this.left = ($aquarium.width() - this.width) * Math.random();
+  this.top = ($aquarium.height() - this.height) * Math.random();
+
   var $node = $('<div class="fish"></div>');
   $node.css('width', this.width);
   $node.css('height', this.height);
-  $node.css('left', (aquariumWidth - this.width) * Math.random());
-  $node.css('top', (aquariumHeight - this.height) * Math.random());
+  $node.css('left', this.left);
+  $node.css('top', this.top);
   return $node;
 };
--- src/init.js
+++ src/init.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   window.fishes = [];
   window.$aquarium = $('#aquarium');
-  for(var i = 0; i < 5; i++) {
+  for(var i = 0; i < 10; i++) {
     window.fishes.push(new Fish());
   }
   for(var i = 0; i < window.fishes.length; i++) {
