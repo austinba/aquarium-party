b03f3fd61de59c9a96ad30ccecdfad4c4a071b8e
Hack Reactor Students <accounts@hackreactor.com>
2016-04-26 00:06:25 -0700

    Added Bob Animation to Fishes

--- src/fish.js
+++ src/fish.js
@@ -1,17 +1,41 @@
 var Fish = function() {
+  this.$node = $('<div class="fish">FISH</div>');
   this.width = 100;
   this.height = 30;
-  this.$node = this.$createNode();
+  this.speed = 50 + 4 * (1 - Math.random()*2); // pixels per second
+  this.direction = Math.random() < 0.5 ? -1 : 1;
+  this.$node.addClass('fish-bob' + Math.floor(Math.random() * 3 + 1));
+  if(this.direction < 0) {
+    this.$node.css('transform', 'scaleX(-1)');
+  }
+  this.left = this.widthAvailable() * Math.random();
+  this.top = this.heightAvailable() * Math.random();
+  this.repaint();
 };
-Fish.prototype.$createNode = function() {
 
-  this.left = ($aquarium.width() - this.width) * Math.random();
-  this.top = ($aquarium.height() - this.height) * Math.random();
+Fish.prototype.widthAvailable = function() {
+  return ($aquarium.width() - this.width);
+}
+Fish.prototype.heightAvailable = function() {
+  return ($aquarium.height() - this.height);
+}
 
-  var $node = $('<div class="fish"></div>');
-  $node.css('width', this.width);
-  $node.css('height', this.height);
-  $node.css('left', this.left);
-  $node.css('top', this.top);
-  return $node;
+Fish.prototype.move = function(interval) {
+  // change direction on edge collison
+  if(this.left < 0) {
+    this.direction = 1;
+    this.$node.css('transform', 'scaleX(1)');
+  } else if(this.left > this.widthAvailable()) {
+    this.direction = -1;
+    this.$node.css('transform', 'scaleX(-1)');
+  }
+  this.left += this.direction * this.speed * interval;
+
+  this.repaint();
 };
+Fish.prototype.repaint = function() {
+  this.$node.css('width', this.width);
+  this.$node.css('height', this.height);
+  this.$node.css('left', this.left);
+  this.$node.css('top', this.top);
+}
--- src/init.js
+++ src/init.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
   window.fishes = [];
+  window.frameRate = 20;
   window.$aquarium = $('#aquarium');
   for(var i = 0; i < 10; i++) {
-    window.fishes.push(new Fish());
+    window.fishes.push(new Fish);
   }
-  for(var i = 0; i < window.fishes.length; i++) {
-    window.fishes[i].$node.appendTo('#aquarium');
-  }
+  window.fishes.forEach(function(fish){
+    setTimeout(function() {
+      fish.$node.appendTo('#aquarium');
+    }, 300);
+    // setTimeout(fish.$node.appendTo.bind(fish.$node, '#aquarium'), 300);
+    // fish.$node.appendTo('#aquarium');
+  });
+  setInterval(refreshScreen, 1000 / window.frameRate);
 });
+
+var refreshScreen = function(){
+  window.fishes.forEach(function(fish){
+    fish.move(1/window.frameRate);
+  });
+}
