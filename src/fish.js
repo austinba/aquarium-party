c4e79ee27d0b316e0edae162a40241b431a709f3
Hack Reactor Students <accounts@hackreactor.com>
2016-04-25 22:56:37 -0700

    Added Background Image Styling in CSS

--- src/fish.js
+++ src/fish.js
@@ -1,12 +1,15 @@
 var Fish = function() {
   this.$node = this.$createNode();
+  this.width = 100;
+  this.height = 30;
 };
 Fish.prototype.$createNode = function() {
-  var windowWidth = $(document.body).width();
-  var windowHeight = $(document.body).height();
+  var aquariumWidth = $aquarium.width();
+  var aquariumHeight = $aquarium.height();
   var $node = $('<div class="fish"></div>');
-  $node.css('left', windowWidth * Math.random());
-  $node.css('top', windowHeight * Math.random());
-  console.log(windowHeight, windowWidth);
+  $node.css('width', this.width);
+  $node.css('height', this.height);
+  $node.css('left', (aquariumWidth - this.width) * Math.random());
+  $node.css('top', (aquariumHeight - this.height) * Math.random());
   return $node;
 };
--- src/init.js
+++ src/init.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
   window.fishes = [];
+  window.$aquarium = $('#aquarium');
   for(var i = 0; i < 5; i++) {
     window.fishes.push(new Fish());
   }
   for(var i = 0; i < window.fishes.length; i++) {
-    window.fishes[i].$node.appendTo(document.body);
+    window.fishes[i].$node.appendTo('#aquarium');
   }
 });
